cf2d0998107f2fc27f7ec08ced7b2ec797b5df47
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-04-09 23:34:12 +0100

    fix: skip database migrations during render build phase

--- V-Interview-Backend/build.sh
+++ V-Interview-Backend/build.sh
@@ -3,4 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+# Migrations should be run in Render's "Release Command" or "Start Command",
+# not during the build phase to avoid database connection issues.
+# python manage.py migrate
