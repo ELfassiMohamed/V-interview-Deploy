f8b40ce27c0ce9f0a9d84135243461c1517dbd55
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-04-10 14:55:20 +0100

    added migration command to build.sh

--- V-Interview-Backend/build.sh
+++ V-Interview-Backend/build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 # Migrations should be run in Render's "Release Command" or "Start Command",
 # not during the build phase to avoid database connection issues.
-# python manage.py migrate
+python manage.py migrate
